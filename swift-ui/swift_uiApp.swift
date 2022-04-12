d9a6fa12f06e471eb53cdf32f5e3476bfaaadfd4
“Daniel <danielg@moxehub.com>
2022-04-12 11:26:04 -0500

    Initial Commit

---
+++ swift-ui/swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_uiApp.swift
+//  swift-ui
+//
+//  Created by Daniel Gutierrez on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
